792007b59ce7cb5ba77c06d2a99ad63e0c4b5c2e
higorsouza <higor.andrade@hotmail.com>
2025-04-04 19:17:21 -0300

    subindo o codigo

---
+++ src/main/java/com/moresales/lambda/Main.java
@@ -0,0 +1,23 @@
+package com.moresales.lambda;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    private float soma(float val1, float val2){
+        return val1+val2;
+    }
+
+    private float subtrair(float val1, float val2){
+        return val1-val2;
+    }
+
+    private float multiplicar(float val1, float val2){
+        return val1*val2;
+    }
+
+    private float dividir(float val1, float val2){
+        return val1/val2;
+    }
+}
